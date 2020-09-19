8f43cd1e0d10ff6c37b1f817eb1476bc43eb1758
AlejandraLujanSosa <54086013+AlejandraLujanSosa@users.noreply.github.com>
2020-09-19 15:21:11 -0300

    primer commit

---
+++ app/src/main/java/prueba/app/firebase/aplicacionlujancita/Operacion.kt
@@ -0,0 +1,21 @@
+package prueba.app.firebase.aplicacionlujancita
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_operacion.*
+
+class Operacion : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_operacion)
+
+        //ConsultarPreciosBtn.setOnClickListener {
+
+            //val intent: Intent = Intent(this, CodigoQR::class.java)
+            //startActivity(intent)
+            //finish()
+        //}
+    }
+}
